3e47ec1ffdb32a9c9d0a0ab1771e875ab4b30eb5
holly3152 <holly67827781@gmail.com>
2020-02-09 23:03:05 +0900

    webtoon_update

---
+++ week2/webtoon_code.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+# 웹페이지 열기
+res = requests.get('https://comic.naver.com/webtoon/weekday.nhn')
+soup = BeautifulSoup(res.text,'html.parser')
+res.close()
+
+# 모든 요일별 웹툰 추출
+data_list = soup.find_all('div', {'class':'col_inner'})
+
+week_lists = []
+
+# 제목 추출
+for data in data_list:
+    title = data.find_all('a', {'class':'title'})
+    titles = [t.text for t in title]
+    week_lists.append(titles)
+
+# 엑셀 파일에 정보 저장
+import pandas as pd
+import numpy as np
+from openpyxl import Workbook
+
+df = pd.DataFrame.from_records(week_lists)
+
+df.to_excel('webtoon.xlsx')
