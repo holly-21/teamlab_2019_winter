98f105487b195bb5d2650d0f8e9f9dcf05aac842
Wonyoungpark <48376471+Wonyoungpark@users.noreply.github.com>
2020-02-21 10:17:15 +0900

    Update settings.py

--- week5/first-django/firstdjango/settings.py
+++ week5/first-django/firstdjango/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import pymysql
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +29,11 @@ SECRET_KEY = '!jr%omey)e&86%tpi12n087*2p9q(56g4_ix3(7^&n#kqhu%q%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '.ap-northeast-2.compute.amazonaws.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
